1a5aee796bb2694875906fa912dfcdb6c9c6354c
cristianexer <dcristian353@gmail.com>
2019-03-20 12:45:13 +0000

    rfidread and request sent

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 import requests as req
 import json
 from base64 import b64decode
 
 
-api_endpoint = "https://attendify.appspot.com/attendance/create"
-
-endpoint = "https://www.google.com"
-
-mock_endpoint = "https://login-mock-xam.herokuapp.com/login"
+api_endpoint = "http://35.197.206.56/attendance/create"
 
 building_name = "INB"
 
@@ -18,33 +18,36 @@ def json_format(di):
    return json.dumps(di, sort_keys=True, indent=4, separators=(',',': '))
 
 def get_student_id(token):
-   base64Url = token.split('.')[1]
-   base64 = base64Url.replace('-','+').replace('_','/')
-   return json.loads(b64decode(base64 + '=' * (-len(base64) % 4)))
-
+   base64 = token.replace('-','+').replace('_','/')
+   return json.loads(b64decode(base64 + '=' * (-len(base64) % 4)))['student_id']
 
 def register_attendance(token):
-   student_id = get_student_id(token)
-   data = {'student_id':student_id, 'building_name':building_name, 'room_id':room_id}
-   headers = {'Authorization': 'Bearer ' + token}
-   res = req.post(url=api_endpoint, headers=headers, data=data)
-   return res
+   good = True
+   try:
+       student_id = get_student_id(token)
+   except:
+       print("No student ID")
+       good = False
+   if good == True:
+       data = {'student_id':student_id, 'building_name':building_name, 'room_id':room_id}
+       headers = {'Authorization': 'Bearer ' + token}
+       res = req.post(url=api_endpoint, headers=headers, data=data)
+       return json.loads(res.text)
+   else:
+       return {'res':'Failed to read student_id'}
 
-def get_page(url):
-   return req.get(url=url, headers={'Accept-Encoding':'identity'})
+reader = SimpleMFRC522()
+
+try:
+    print("Tap your phone or card")
+    id, token = reader.read()
+    print(id)
+    print(token)
+    res = register_attendance(token)
+    print(res['response'])
+finally:
+    GPIO.cleanup()
 
 
-def send_mock_req(payload):
-  return req.post(url=mock_endpoint,headers={'Content-Type':'application/json'} , data=payload)
-
-temp = { 'email':'test1@gmail.com', 'password':'test'}
-
-login_mock = send_mock_req(json_format(temp))
-
-response = json.loads(login_mock.text)
-
-s_id = get_student_id(response['token'])['user_id']
-
-print(s_id)
 
 
